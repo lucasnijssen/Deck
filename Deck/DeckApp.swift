2ff3cb0873a34855f426c9a093854dd5db72aefe
Lucasnijssen <Nijssenlucas@gmail.com>
2026-03-28 14:35:20 +0100

    Initial Commit

---
+++ Deck/DeckApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeckApp.swift
+//  Deck
+//
+//  Created by Lucas Nijssen on 3/28/26.
+//
+
+import SwiftUI
+
+@main
+struct DeckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
